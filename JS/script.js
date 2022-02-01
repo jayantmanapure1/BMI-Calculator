a3e72aa3606e2faf2491813282408e412013537e
001jayant <jayantmanapure1@gmail.com>
2022-02-01 19:12:16 +0530

    Inital  commit

---
+++ JS/script.js
@@ -0,0 +1,37 @@
+function calculate(){
+    
+      var bmi;
+      var result = document.getElementById("result");
+     var height = parseInt(document.getElementById("height").value);
+    
+      var weight = parseInt(document.getElementById("weight").value);
+
+      document.getElementById("weight-val").textContent = weight + " kg";
+      document.getElementById("height-val").textContent = height + " cm";
+    
+      bmi = (weight / Math.pow( (height/100), 2 )).toFixed(1);
+      result.textContent = bmi;
+  
+    
+   
+      if(bmi < 18.5){
+          category = "Underweight 😒";
+          result.style.color = "#ffc44d";
+      }
+      else if( bmi >= 18.5 && bmi <= 24.9 ){
+          category = "Normal Weight 😍";
+          result.style.color = "#0be881";
+      }
+    
+    
+      else if( bmi >= 25 && bmi <= 29.9 ){
+          category = "Overweight 😮";
+          result.style.color = "#ff884d";
+      }
+    
+      else{
+          category = "Obese 😱";
+          result.style.color = "#ff5e57";
+      }
+      document.getElementById("category").textContent = category;
+  }
